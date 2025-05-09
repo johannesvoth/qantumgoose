b5d259ac4d368e961178b0b76de2da9a9ee0d878
johannesvoth <johannes.voth3@gmail.com>
2025-05-09 14:47:42 +0200

    whater

--- player/player.gd
+++ player/player.gd
@@ -5,7 +5,6 @@ extends CharacterBody2D
 var movement_intent := Vector2.ZERO # aka direction
 var jump_intent := false
 
-@onready var player_input: MultiplayerSynchronizer = $PlayerInput
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(int(str(name)))
@@ -15,14 +14,30 @@ func _physics_process(delta):
 		return 
 	
 	movement_intent = Input.get_vector("left", "right", "up", "down")
+	jump_intent = Input.is_action_just_pressed("jump")
+	
 	velocity = movement_intent * SPEED
 	move_and_slide()
 	
 	if jump_intent:
 		do_jump()
 
-@rpc("call_local")
 func do_jump():
 	jump_intent = false
 	print("jumped")
-	Events.spawn_random_sprite.emit()
+	spawn_random_sprite()
+
+
+const BUCKET = preload("res://bucket.tscn")
+func spawn_random_sprite():
+	print("spawned random sprite")
+	var rand_color = get_random_color()
+	var bucket_instance = BUCKET.instantiate()
+	add_child(bucket_instance, true)
+	bucket_instance.modulate = rand_color
+
+func get_random_color() -> Color:
+	var r = randf()
+	var g = randf()
+	var b = randf()
+	return Color(r, g, b)
--- world/world.gd
+++ world/world.gd
@@ -4,15 +4,6 @@ extends Node2D
 func _ready() -> void:
 	var rand_color = get_random_color()
 	sprite_2d.modulate = rand_color
-	
-	Events.spawn_random_sprite.connect(_on_spawn_random_sprite)
-
-const BUCKET = preload("res://bucket.tscn")
-func _on_spawn_random_sprite():
-	var rand_color = get_random_color()
-	var bucket_instance = BUCKET.instantiate()
-	add_child(bucket_instance, true)
-	bucket_instance.modulate = rand_color
 
 func get_random_color() -> Color:
 	var r = randf()
