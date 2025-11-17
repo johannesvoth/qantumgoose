1b7eb2724020283ed4bbaef734881b5559e76c21
Johannes Voth <johannes.voth3@gmail.com>
2025-11-17 03:02:01 +0100

    new update

--- other/steam.gd
+++ other/steam.gd
@@ -2,24 +2,30 @@ extends Node
 
 signal join(int)
 
-# this makes the game boot up as SpaceWar on steam
-
 func _ready():
+	# Initialize Steam (using SpaceWar app ID 480 for testing)
 	OS.set_environment("SteamAppId", str(480))
 	OS.set_environment("SteamGameId", str(480))
-	var a = Steam.steamInitEx(false)
+	
+	var init_result = Steam.steamInitEx(false)
+	print("Steam initialization: ", init_result)
+	
+	# Initialize relay network access for P2P connections
+	Steam.initRelayNetworkAccess()
+	
+	# Connect signals
 	Steam.lobby_invite.connect(lobby_invite)
 	Steam.lobby_joined.connect(lobby_joined)
 	Steam.join_requested.connect(join_requested)
 
 func lobby_invite(inviter: int, lobby: int, game: int):
-	print("steam lobby invite")
+	print("Steam lobby invite from user: ", inviter)
 
 func lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
-	print("Lobby Joined")
+	print("Lobby joined: ", lobby, " Response: ", response)
 
 func join_requested(lobby_id: int, steam_id: int):
-	print("join requested")
+	print("Join requested - Lobby: ", lobby_id, " from user: ", steam_id)
 	join.emit(lobby_id)
 
 func _process(delta):
--- ui/multiplayer_ui.gd
+++ ui/multiplayer_ui.gd
@@ -2,48 +2,68 @@ extends Control
 
 @onready var multiplayer_ui: Control = $"."
 @onready var lobby_list: VBoxContainer = $SteamStuff/ScrollContainer/LobbyList
-
 @onready var text_edit_ip_adress_lan: TextEdit = $LANStuff/TextEditIPAdressLAN
 @onready var text_edit_port_lan: TextEdit = $LANStuff/TextEditPortLAN
 
 const PLAYER = preload("res://player/player.tscn")
 
 @onready var player_spawner: MultiplayerSpawner = $"../PlayerSpawner"
-
+@onready var world_spawner: MultiplayerSpawner = $"../WorldSpawner"
 
 var lobby_id: int = 0
 var steam_peer = SteamMultiplayerPeer.new()
 var lan_peer = ENetMultiplayerPeer.new()
 
 func _ready():
-	# Yeah so we need to do this here because the Steam autoload doesn't call it for some reason. I don't like it.
-	steam_peer.lobby_created.connect(_on_lobby_created)
+	# Connect Steam signals
+	Steam.lobby_created.connect(_on_lobby_created)
 	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	Steam.lobby_joined.connect(_on_lobby_joined)
 
+func _on_steam_join(lobby_id: int):
+	Steam.joinLobby(lobby_id)
 
-
-func _on_steam_join(lobby_id:int): # no adding of player here, but when? = when the host is created, he setupts the signal for when other peers join.
-	steam_peer.connect_lobby(lobby_id)
-	multiplayer.multiplayer_peer = steam_peer
+func _on_lobby_joined(lobby_id: int, _permissions: int, _locked: bool, _response: int) -> void:
+	# Check if we're the owner (host) - if so, ignore this signal
+	if Steam.getLobbyOwner(lobby_id) == Steam.getSteamID():
+		return
 	
+	# We're joining someone else's lobby
+	steam_peer.connect_to_lobby(lobby_id)
+	multiplayer.multiplayer_peer = steam_peer
 	multiplayer_ui.hide()
 
-
-func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
-	if connect == 1:
+func _on_lobby_created(result: int, this_lobby_id: int) -> void:
+	if result == Steam.Result.RESULT_OK:
 		lobby_id = this_lobby_id
-		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName()+"'s Lobby"))
+		Steam.setLobbyData(lobby_id, "name", str(Steam.getPersonaName() + "'s Lobby"))
 		Steam.setLobbyJoinable(lobby_id, true)
-		print("Created lobby: ",str(Steam.getPersonaName()+"'s Lobby"))
+		print("Created lobby: ", str(Steam.getPersonaName() + "'s Lobby"))
+		
+		# Now host with this lobby
+		steam_peer.host_with_lobby(lobby_id)
+		multiplayer.multiplayer_peer = steam_peer
+		multiplayer_ui.hide()
+		
+		# Setup peer connection signal
+		multiplayer.peer_connected.connect(
+			func(p_id):
+				print(str(p_id) + " has joined the Steam game")
+				add_player(p_id)
+		)
+		
+		# Spawn world and add host player
+		world_spawner.spawn_world()
+		add_player(multiplayer.get_unique_id())
 
 func _on_lobby_match_list(these_lobbies: Array) -> void:
-	
+	# Clear existing lobby buttons
 	if lobby_list.get_child_count() > 0:
 		for n in lobby_list.get_children():
 			n.queue_free()
 	
+	# Create button for each lobby
 	for this_lobby in these_lobbies:
-		
 		var lobby_name: String = Steam.getLobbyData(this_lobby, "name")
 		var lobby_mode: String = Steam.getLobbyData(this_lobby, "mode")
 		var lobby_num_members: int = Steam.getNumLobbyMembers(this_lobby)
@@ -53,7 +73,6 @@ func _on_lobby_match_list(these_lobbies: Array) -> void:
 		lobby_button.set_size(Vector2(400/4, 25/4))
 		lobby_button.set_name("lobby_%s" % this_lobby)
 		lobby_button.connect("pressed", Callable(self, "_on_steam_join").bind(this_lobby))
-
 		lobby_list.add_child(lobby_button)
 
 func _on_refresh_lobbies_button_pressed() -> void:
@@ -61,22 +80,10 @@ func _on_refresh_lobbies_button_pressed() -> void:
 	print("305 Mr. Worldwide Requesting a lobby list")
 	Steam.requestLobbyList()
 
-@onready var world_spawner: MultiplayerSpawner = $"../WorldSpawner"
-
 func _on_host_steam_pressed() -> void:
-	steam_peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC)
-	multiplayer.multiplayer_peer = steam_peer
-	multiplayer_ui.hide()
-	
-	multiplayer.peer_connected.connect(
-		func(p_id):
-			print(str(p_id) + "has joined the LAN game")
-			add_player(p_id)
-	)
-	
-	world_spawner.spawn_world()
-	add_player(multiplayer.get_unique_id()) # always host but whatever
-
+	# Create lobby using Steam's createLobby (not the peer's method)
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, 16)  # 16 max players
+	# The rest happens in _on_lobby_created callback
 
 func _on_host_lan_pressed() -> void:
 	var port = text_edit_port_lan.text
@@ -85,25 +92,22 @@ func _on_host_lan_pressed() -> void:
 	
 	multiplayer.peer_connected.connect(
 		func(p_id):
-			print(str(p_id) + "has joined the LAN game")
+			print(str(p_id) + " has joined the LAN game")
 			add_player(p_id)
 	)
 	
-	add_player(multiplayer.get_unique_id()) # host player
-	
-	world_spawner.spawn_world() # only spawn the world if you are a host
-	
+	add_player(multiplayer.get_unique_id())
+	world_spawner.spawn_world()
 	multiplayer_ui.hide()
 
 func _on_join_lan_pressed() -> void:
 	var port = text_edit_port_lan.text
 	var hostname = text_edit_ip_adress_lan.text
-	lan_peer.create_client(hostname,int(port))
+	lan_peer.create_client(hostname, int(port))
 	multiplayer.multiplayer_peer = lan_peer
-	
 	multiplayer_ui.hide()
 
-func add_player(p_id): # just done it here instead of giving the player spawner a dedicated script. Might move this in the future.
+func add_player(p_id):
 	var player = PLAYER.instantiate()
 	player.name = str(p_id)
 	player_spawner.add_child(player, true)
